936cc58c982c024ba8e0a253b732268b76722ae4
QuentinGaillot <gaillot.q@gmail.com>
2018-05-01 17:46:58 -0400

    Classification avec thread pour le graph, environ 1.5 secondes par images

--- gender_age_server.py
+++ gender_age_server.py
@@ -3,7 +3,6 @@
 from gender_age_service.srv import *
 import node_object
 
+#creation de l'objet node qui contient le service
 if __name__ == "__main__":
     n = node_object.NodePrediction()
-
-
--- prediction_age_gender.py
+++ prediction_age_gender.py
@@ -10,30 +10,33 @@ from utils import *
 import matplotlib.pyplot as plt
 import rospkg
 
-
-def prediction(image_message, loaded_model, g1):
+def prediction(image, loaded_model, g1):
     input_size = 416
     max_box_per_image = 10
     anchors = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]
     labels = ["face"]
-    # image_path   = "image/olivier.jpg"
 
     # DEBUT ANALYSE DE L'IMAGE
-
+    #de l'image message au format lisible pour le prog
     bridge = CvBridge()
-    image = bridge.imgmsg_to_cv2(image_message.image)
-    image2 = cv2.resize(image, (input_size, input_size))
+    image_cv = bridge.imgmsg_to_cv2(image)
+    image2 = cv2.resize(image_cv, (input_size, input_size))
     image2 = normalize(image2)
     input_image = image2[:, :, ::-1]
     input_image = np.expand_dims(input_image, 0)
-    dummy_array = dummy_array = np.zeros((1, 1, 1, 1, max_box_per_image, 4))
+    dummy_array = np.zeros((1, 1, 1, 1, max_box_per_image, 4))
     input_data = [input_image, dummy_array]
+
+    #detection des visages
     netout = loaded_model.predict([input_image, dummy_array])[0]
     boxes = decode_netout2(netout, labels, anchors)
 
+    #si au moins un visage est detecte
     if len(boxes) != 0:
-        listImg = getFacesList(image, boxes)
+        listImg = getFacesList(image_cv, boxes)
+        #classification homme-femme et age
         listPrediction = g1.classify_age(listImg)
+    #sinon retourne vide
     else:
         listPrediction = None
 
